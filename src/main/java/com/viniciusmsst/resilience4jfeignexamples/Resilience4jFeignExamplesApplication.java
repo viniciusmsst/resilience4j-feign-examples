999b8843403c9e5b03995aeb185079916aea6668
Vinícius Santos <viniciusmsst@gmail.com>
2020-05-09 11:44:37 -0300

    Initial commit

---
+++ src/main/java/com/viniciusmsst/resilience4jfeignexamples/Resilience4jFeignExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusmsst.resilience4jfeignexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jFeignExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4jFeignExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viniciusmsst/resilience4jfeignexamples/Resilience4jFeignExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusmsst.resilience4jfeignexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jFeignExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
